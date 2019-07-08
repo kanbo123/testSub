6c8e78bee23c08f4137a6e9296b40baa33e030bd
kanbo123 <52125963+kanbo123@users.noreply.github.com>
2019-07-08 12:51:27 +0800

    Update UserLoadBalance.java

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -45,12 +45,12 @@ public class UserLoadBalance implements LoadBalance{
                 int weight = serverLoadInfo.getWeight();
                 if(permits > 0 ){
                     //根据耗时重新计算权重(基本权重*(1秒/单个请求耗时))
-//                    int clientTimeAvgSpendCurr = serverLoadInfo.getClientTimeAvgSpentTps();
-//                    if(clientTimeAvgSpendCurr ==0 ){
-//                        // 没有请求数据
-//                        return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
-//                    }
-//                    weight = weight*(1000/clientTimeAvgSpendCurr);
+                    int clientTimeAvgSpendCurr = serverLoadInfo.getClientTimeAvgSpentTps();
+                    if(clientTimeAvgSpendCurr ==0 ){
+                        // 没有请求数据
+                        return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
+                    }
+                    weight = weight*(1000/clientTimeAvgSpendCurr);
                     hasPermitArr.add(index);
                     weightArr.add(weight);
                     totalWeight = totalWeight+weight;
